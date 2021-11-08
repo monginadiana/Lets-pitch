1262e2f3d5619b713716b18caeecbdd8aea1fdf0
monginadiana <diana.ondieki@student.moringa.com>
2021-11-08 16:04:50 +0300

    creation of the login render template in the auths views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
-from app.models import User
+from flask import render_template
 from . import auth
-from flask import render_template,redirect,url_for, flash,request
-# from flask_login import login_user, logout_user, login_required
-from .forms import RegistrationForm
+from flask import render_template,redirect,url_for,flash,request
+from ..models import User
+from .forms import RegistrationForm,LoginForm
 from .. import db
+from flask_login import login_user,logout_user,login_required
 from ..email import mail_message
 
 
@@ -21,3 +22,16 @@ def register():
     title = "SignUp | PitchDeck"
     return render_template('auth/register.html',registration_form = form,title = title)
 
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.loggedin'))
+
+        flash('Invalid username or Password')
+
+    title = "SignIn | PitchDeck"
+    return render_template('auth/login.html',login_form = login_form,title=title)
